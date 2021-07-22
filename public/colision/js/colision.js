371fd35c8d82a7ff508d76d46d6195725df4916d
George C <anceun.1995@gmail.com>
2021-07-22 16:01:18 -0500

    Colision

---
+++ public/colision/js/colision.js
@@ -0,0 +1,102 @@
+var canvas = document.getElementById('canvas');
+var context = canvas.getContext('2d');
+
+window.onload = function() {
+    document.addEventListener('keydown', function(e) {
+        if(e.key == 37) {
+            j.keyLeft = true;
+        }
+        if(e.key == 38) {
+            j.keyRight = true;
+        }
+        if(e.key == 39) {
+            j.keyUp = true;
+        }
+        if(e.key == 40) {
+            j.keyDown = true;
+        }
+    })
+
+    document.addEventListener('keyup', function(e) {
+        if(e.key == 37) {
+            j.keyLeft = false;
+        }
+        if(e.key == 38) {
+            j.keyRight = false;
+        }
+        if(e.key == 39) {
+            j.keyUp = false;
+        }
+        if(e.key == 40) {
+            j.keyDown = false;
+        }
+    })
+
+    j = new Player();
+    w = new Wall();
+    loop();
+}
+
+function Player() {
+    this.x = 190;
+    this.y = 170;
+    this.w = 80;
+    this.h = 50;
+    this.vel = 5;
+    this.color = "black";
+
+    this.keyLeft = false;
+    this.keyRight = false;
+    this.keyUp = false;
+    this.keyDown = false;
+
+
+    this.draw = function() {
+        context.fillStyle = this.color;
+        context.fillRect(this.x,this.y,this.w,this.h);
+    }
+
+    this.move = function() {
+        if(this.keyLeft == true) {
+            this.x -= this.vel;
+        }
+        if(this.keyRight == true) {
+            this.x += this.vel;
+        }
+        if(this.keyUp == true) {
+            this.y -= this.vel;
+        }
+        if(this.keyDown == true) {
+            this.y += this.vel;
+        }
+    }
+}
+
+function Wall() {
+    this.x = 200;
+    this.y = 200;
+    this.w = 80;
+    this.h = 50;
+    this.color = "green";
+
+    this.draw = function() {
+        context.fillStyle = this.color;
+        context.fillRect(this.x,this.y,this.w,this.h);
+    }
+}
+
+function colision() {
+    if(j.x < w.x + w.w && j.x + j.w > w.x 
+        && j.y < w.y + w.h && j.y+j.h > w.y) {
+            window.alert("Chocamos!");
+    }
+}
+
+function loop() {
+    //canvas.width = canvas.witdh;
+    j.draw();
+    w.draw();
+    j.move();
+    colision();
+    requestAnimationFrame(loop);
+}
---
+++ resources/views/colision.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Colision</title>
+        <link href="style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <canvas id="canvas" width="500px" height="500px" style="border: 1px solid black;"> </canvas>
+        <script type="text/javascript" src="{{ URL::to('colision/js/colision.js') }}"></script>
+        <h3>Se modifica manualmente la posicion del rectangulo negro para que se choque. </h3>
+    </body>
+</html>
